54b3b9c8da0df6c6a115aa7b5c2b54bf4987af11
vaspean <vaspean@mail.ru>
2022-01-19 20:11:59 +0300

    pagination add

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-document.addEventListener("DOMContentLoaded", async function(event) {
-    users = await fetchUsers();
-    console.log(users);
+document.addEventListener("DOMContentLoaded", async () => {
+    const USERS_PER_PAGE = 10;
 
-    updateTable(users);
+    users = await fetchUsers();
+
+    const pagesCount = users.length / USERS_PER_PAGE;
+    let currentPage = pageByURL(pagesCount);
+
+    updatePaginationElements(currentPage, pagesCount);
+
+    const previousPageButton = document.querySelector(".pagination__button_previous-page");
+    const nextPageButton = document.querySelector(".pagination__button_next-page");
+
+    previousPageButton.addEventListener("click", function() {
+        if (currentPage > 1) {
+            currentPage--;
+            paginate(currentPage);
+        }
+    });
+
+    nextPageButton.addEventListener("click", function() {
+        if (currentPage < pagesCount) {
+            currentPage++;
+            paginate(currentPage);
+        }
+    });
+
+    updateTable(usersByPage(users, currentPage, USERS_PER_PAGE));
 });
--- js/methods.js
+++ js/methods.js
@@ -1,31 +1,45 @@
 async function fetchUsers() {
-    const response = await fetch('../users.json');
+    const response = await fetch("../users.json");
     const users = await response.json();
     return users;
 }
 
+function pageByURL(pagesCount) {
+    const START_PAGE_NUMBER = 1;
+    const paramsString = window.location.search;
+    const searchParams = new URLSearchParams(paramsString);
+    let currentPage;
+
+    if (searchParams.get("page") === null || +searchParams.get("page") < 1 || +searchParams.get("page") > pagesCount) {
+        currentPage = START_PAGE_NUMBER;
+    } else {
+        currentPage = +searchParams.get("page");
+    }
+    return currentPage;
+}
+
 function clearTable() {
-    let table = document.querySelector('.table__tbody');
-    let userRow = document.querySelectorAll('.user-row');
+    const table = document.querySelector(".table__tbody");
+    const userRow = document.querySelectorAll(".user-row");
     for (let i = 0; i < userRow.length; i++) {
         table.removeChild(userRow[i]);
     }
 }
 
 function updateTable(users) {
-    clearTable();
-    let tableBody = document.querySelector('.table__tbody');
-    for (let user of users) {
-        let tr = document.createElement('tr');
-        let tdFirstName = document.createElement('td');
-        let tdLastName = document.createElement('td');
-        let tdAbout = document.createElement('td');
-        let pAbout = document.createElement('p');
-        let tdEyeColor = document.createElement('td');
-        tdFirstName.classList.add('first-name');
-        tdLastName.classList.add('last-name');
-        tdAbout.classList.add('about');
-        tdEyeColor.classList.add('eye-color');
+    // clearTable();
+    const tableBody = document.querySelector(".table__tbody");
+    for (const user of users) {
+        const tr = document.createElement("tr");
+        const tdFirstName = document.createElement("td");
+        const tdLastName = document.createElement("td");
+        const tdAbout = document.createElement("td");
+        const pAbout = document.createElement("p");
+        const tdEyeColor = document.createElement("td");
+        tdFirstName.classList.add("first-name");
+        tdLastName.classList.add("last-name");
+        tdAbout.classList.add("about");
+        tdEyeColor.classList.add("eye-color");
         tdFirstName.innerHTML = user.name.firstName;
         tdLastName.innerHTML = user.name.lastName;
         pAbout.innerHTML = user.about;
@@ -37,4 +51,38 @@ function updateTable(users) {
         tdAbout.appendChild(pAbout);
         tr.appendChild(tdEyeColor);
     }
+}
+
+function usersByPage(users, page, usersByPage) {
+    const offset = (page - 1) * usersByPage;
+    return users.slice(offset, offset + usersByPage);
+}
+
+function updatePaginationElements(currentPage, pageCount) {
+    const paginationContainer = document.querySelector(".pagination");
+    const previousPageButton = document.querySelector(".pagination__button_previous-page");
+    const nextPageButton = document.querySelector(".pagination__button_next-page");
+    for (let i = 1; i <= pageCount; i++) {
+        const pageButton = document.createElement("button");
+        pageButton.classList.add('pagination__button');
+        pageButton.innerHTML = i;
+        if (currentPage === i) {
+            pageButton.classList.add("pagination__button_current-page");
+        }
+        pageButton.addEventListener("click", () => {
+            paginate(i);
+        })
+        paginationContainer.insertBefore(pageButton, nextPageButton);
+    }
+
+    if (currentPage <= 1) {
+        previousPageButton.setAttribute("disabled", "disabled");
+    }
+    if (currentPage >= pageCount) {
+        nextPageButton.setAttribute("disabled", "disabled");
+    }
+}
+
+function paginate(currentPage) {
+    document.location.href = `${window.location.pathname}?page=${currentPage}`;
 }
